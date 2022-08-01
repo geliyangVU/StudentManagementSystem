4ca51a824aae0c2d25893d2796cab74de929d06a
Geli Yang <gyang5@villanova.edu>
2022-08-01 19:06:32 -0400

    implement getStudentById method in StudentService

--- src/main/java/com/example/springmysqljpashiroproject/service/StudentService.java
+++ src/main/java/com/example/springmysqljpashiroproject/service/StudentService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -39,5 +40,10 @@ public class StudentService {
         return (List<Student>) studentDao.findAll();
     }
 
+    
+
+    public Optional<Student> getStudentById(Long id){
+        return studentDao.findById(id);
+    }
 
 }
