8330ca12ef821bf3502366a64ccfb6dfcb1e950f
Geli Yang <gyang5@villanova.edu>
2022-08-01 16:26:46 -0400

    Initial spring initializer commit

---
+++ src/main/java/com/example/springmysqljpashiroproject/SpringMySqljpaShiroProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmysqljpashiroproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMySqljpaShiroProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMySqljpaShiroProjectApplication.class, args);
+    }
+
+}
