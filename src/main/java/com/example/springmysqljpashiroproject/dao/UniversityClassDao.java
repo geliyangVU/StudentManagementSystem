3ed8a1428f8d282973f38153620c53efdc20302b
Geli Yang <gyang5@villanova.edu>
2022-08-01 20:45:23 -0400

    Let UniversityClassDao extends CrudRepository from Spring framework

---
+++ src/main/java/com/example/springmysqljpashiroproject/dao/UniversityClassDao.java
@@ -0,0 +1,8 @@
+package com.example.springmysqljpashiroproject.dao;
+
+import com.example.springmysqljpashiroproject.model.UniversityClass;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UniversityClassDao extends CrudRepository<UniversityClass, Long> {
+
+}
