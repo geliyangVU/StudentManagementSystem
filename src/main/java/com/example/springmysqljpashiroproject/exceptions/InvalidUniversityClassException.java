e712e8424006312e67496fdef91e7d2cf93bf40b
Geli Yang <gyang5@villanova.edu>
2022-08-03 01:20:20 -0400

    Implemented addClass service and InvalidUniversityClassException handling methods

---
+++ src/main/java/com/example/springmysqljpashiroproject/exceptions/InvalidUniversityClassException.java
@@ -0,0 +1,9 @@
+package com.example.springmysqljpashiroproject.exceptions;
+
+public class InvalidUniversityClassException extends RuntimeException {
+
+    public InvalidUniversityClassException(String message){
+        super(message);
+    }
+
+}
--- src/main/java/com/example/springmysqljpashiroproject/service/UniversityClassService.java
+++ src/main/java/com/example/springmysqljpashiroproject/service/UniversityClassService.java
@@ -2,10 +2,12 @@ package com.example.springmysqljpashiroproject.service;
 
 
 import com.example.springmysqljpashiroproject.dao.UniversityClassDao;
+import com.example.springmysqljpashiroproject.exceptions.InvalidUniversityClassException;
 import com.example.springmysqljpashiroproject.model.UniversityClass;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Calendar;
 import java.util.List;
 
 @Service
@@ -16,10 +18,22 @@ public class UniversityClassService {
     public UniversityClassService(UniversityClassDao universityClassDao) {
         this.universityClassDao = universityClassDao;
     }
-    
+
     public List<UniversityClass> getAllClasses(){
         return (List<UniversityClass>) universityClassDao.findAll();
     }
 
+
+    public UniversityClass addClass(UniversityClass universityClass){
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        if(universityClass.getYear()<currentYear){
+            throw new InvalidUniversityClassException("Can not add class for past year");
+        }
+        if(universityClass.getYear()>currentYear+4){
+            throw new InvalidUniversityClassException("Can not add class for years in future ");
+        }
+
+        return universityClassDao.save(universityClass);
+    }
 
 }
